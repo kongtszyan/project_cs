504904721120fb656aa8fe321e52d08d18b59fba
kongtszyan <132253787+kongtszyan@users.noreply.github.com>
2023-05-11 00:38:18 +0800

    Add files via upload

---
+++ c_quiz_question.js
@@ -0,0 +1,24 @@
+ /*  {
+    "question": "What is the scientific name of a butterfly?",
+    "answers": [
+    "Apis",
+    "Coleoptera",
+    "Formicidae",
+    "Rhopalocera"
+    ],
+    "correctIndex": 3
+}*/
+/*let q = {"Question": "Which approach would you adopt as an HR manager during a job interview?"};
+let ans_txt = '{"Correct_ans":"Ask standarized questions for all candidates", "Incorrect_ans1":"Ask questions that encourage the candidates towards a desired answer"", "Incorrect_ans2":"Ask questions based on your preliminary assumptions", "Incorrect_ans3": "Ask questions that come to your mind" }';
+let ans = JSON.parse(ans_txt);*/
+//question = document.getElementByClassName("background").getElementById("display-container").getElementById("question").getElementById("header");
+
+//fetch("c_quiz_question.json")
+//const obj = JSON.parse('{[{"question": "Which approach would you adopt as an HR manager during a job interview?", "correctAns": "Ask standarized questions for all candidates", "incorrectAns1": "Ask questions that encourage interviewee towards a desired answer", "incorrectAns2": "Ask questions based on your preliminary assumptions", "incorrectAns3": "Ask questions that come to your mind"}]}');
+question = document.querySelector("#header").innerHTML = "Y";
+choice1 = document.getElementById("option-1").innerHTML = "Y";
+choice2 = document.getElementById("option-2").innerHTML = "Y";
+choice3 = document.getElementById("option-3").innerHTML = "Y";
+choice4 = document.getElementById("option-4").innerHTML = "Yweofnewrpovkneprjgeprvmsfwkefjpwejfpwejfpsdjfpsdjflsjflsjflsjflsfjlksdflsdflksdflksdfmlsdfsdeprmbpermbperob pe neprjfpowekf[woekfpfsdjfhaslfjslfjspdfjsdfwejfpwejfpwofw";
+
+//choiceCollection[0].innerHTML = ans[1];
